2fda9e6558c10f4e249541f30058d4c097f805ef
alkanaziz <alkanaziz@gmail.com>
2024-03-25 12:46:23 +0100

    Refactor layout and navbar

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        {children}
+        <main className="container min-h-screen mx-auto px-4">{children}</main>
       </body>
     </html>
   );
--- components/Navbar.js
+++ components/Navbar.js
@@ -2,22 +2,24 @@ import Link from "next/link"
 
 const Navbar = () => {
     const links = [
-        { href: "/", label: "Home" },
-        { href: "/projects", label: "Projects" },
-        { href: "/dashboard", label: "Dashboard" },
-        { href: "/contact", label: "Contact" },
-        { href: "/about", label: "About" },
+        { href: "/", label: "home" },
+        { href: "/projects", label: "projects" },
+        { href: "/dashboard", label: "dashboard" },
+        { href: "/contact", label: "contact" },
+        { href: "/about", label: "about" },
     ]
     return (
         <div>
-            <nav className="flex justify-between items-center bg-slate-800 p-2">
+            <nav className="container mx-auto px-4 flex justify-between items-center bg-slate-800 p-2">
                 <div>
-                    <h1 className="text-slate-200 text-2xl font-bold">Next.js Workshop</h1>
+                    <h1 className="text-slate-200 text-2xl font-bold">
+                        <Link href={"/"}>Next.js WS</Link>
+                    </h1>
                 </div>
                 <ul className=" flex justify-end gap-3">
                     {links.map((link) => {
                         return (
-                            <li key={link.href}><Link className="bg-slate-200 rounded-lg px-2 py-1" href={link.href}>{link.label}</Link></li>
+                            <li key={link.href}><Link className="bg-slate-200 rounded-lg px-2 py-1 hover:bg-slate-300 capitalize" href={link.href}>{link.label}</Link></li>
                         )
                     })}
                 </ul>
